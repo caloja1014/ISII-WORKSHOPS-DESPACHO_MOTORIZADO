50f9498dcad6943a6315dfe7a0f1ccfbbf0fdc90
NataliaMw <mnmawyin@espol.edu.ec>
2021-10-18 13:39:12 -0500

    getters and setters

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,20 +1,46 @@
 package ec.edu.espol.workshops;
 
-enum Sex{
-	MALE,
-	FEMALE
+import ejemploS.MaritalStatus;
+import ejemploS.Sex;
+
+enum Sex {
+	MALE, FEMALE
 }
-enum MaritalStatus{
-	MARRIED,
-	NOTMARRIED
+
+enum MaritalStatus {
+	MARRIED, NOTMARRIED
 }
 
 public class CarInsurance {
 	private int basePremium;
 	private Sex sex;
 	private MaritalStatus maritalStatus;
-	
-	public CarInsurance () {
-		this.basePremium=500;
-	}	
+
+	public CarInsurance() {
+		this.basePremium = 500;
+	}
+
+	public int getBasePremium() {
+		return basePremium;
+	}
+
+	public void setBasePremium(int basePremium) {
+		this.basePremium = basePremium;
+	}
+
+	public Sex getSex() {
+		return sex;
+	}
+
+	public void setSex(Sex sex) {
+		this.sex = sex;
+	}
+
+	public MaritalStatus getMaritalStatus() {
+		return maritalStatus;
+	}
+
+	public void setMaritalStatus(MaritalStatus maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
 }
