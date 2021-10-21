ebe43a93d6fb69b2972b51cbe648380003e2bce8
Jaime <japibaro@espol.edu.ec>
2021-10-21 14:29:38 -0500

    Fixing some PMD violations

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Main.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Main.java
@@ -1,8 +1,14 @@
 package ec.edu.espol.workshops;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+/**
+ * 
+ * @author Jaime
+ *  
+ */
+public class Main { 
 	
 	public static void main(String args[]) {
 		Scanner scanObj = new Scanner(System.in);
@@ -11,12 +17,12 @@ public class Main {
 		
 		System.out.println("Enter your sex M for Male and F for Female");
 		scanObj.nextLine();
-		String sex = scanObj.nextLine().toUpperCase();
+		String sex = scanObj.nextLine().toUpperCase(Locale.getDefault());
 		Sex s = sex.equals("M") ? Sex.MALE : Sex.FEMALE;
 		
 		System.out.println("Are you married or not? (Y)/(N)");
-		String isMarried = scanObj.nextLine().toUpperCase();
-		MaritalStatus marrSt = isMarried.equals("Y") ? MaritalStatus.MARRIED : MaritalStatus.NOTMARRIED;
+		String isMarried = scanObj.nextLine().toUpperCase(Locale.getDefault());
+		MaritalStatus marrSt = "Y".equals(isMarried) ? MaritalStatus.MARRIED : MaritalStatus.NOTMARRIED;
 		
 		CarInsurance ci = new CarInsurance(s,marrSt,age);
 		ci.setPremiumIns();
