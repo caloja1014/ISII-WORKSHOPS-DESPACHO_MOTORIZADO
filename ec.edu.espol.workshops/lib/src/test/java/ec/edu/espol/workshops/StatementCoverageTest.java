3176d95b0bdff2ff3d92ffcd7bf94d36f36592f0
caloja1014 <caloja@espol.edu.ec>
2022-01-03 13:21:55 -0500

    test

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StatementCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StatementCoverageTest.java
@@ -6,51 +6,51 @@ import static org.junit.Assert.assertEquals;
 public class StatementCoverageTest {
 
 	
-//	@Test
-//	public void test001(){
-//		Sex s= Sex.MALE;
-//		MaritalStatus marrSt = MaritalStatus.MARRIED;
-//		int age =45;
-//		CarInsurance ci = new CarInsurance(s,marrSt,age);
-//		ci.setPremiumIns();
-//		
-//		int premium = ci.getBasePremium();
-//		assertEquals(premium,400);
-//	}
-//	
-//	@Test
-//	public void test002(){
-//		Sex s= Sex.MALE;
-//		MaritalStatus marrSt = MaritalStatus.MARRIED;
-//		int age =35;
-//		CarInsurance ci = new CarInsurance(s,marrSt,age);
-//		ci.setPremiumIns();
-//		
-//		int premium = ci.getBasePremium();
-//		assertEquals(premium,300);
-//	}
-//	
-//	@Test
-//	public void test003(){
-//		Sex s= Sex.MALE;
-//		MaritalStatus marrSt = MaritalStatus.NOTMARRIED;
-//		int age =24;
-//		CarInsurance ci = new CarInsurance(s,marrSt,age);
-//		ci.setPremiumIns();
-//		
-//		int premium = ci.getBasePremium();
-//		assertEquals(premium,2000);
-//	}
-//	
-//	@Test
-//	public void test004(){
-//		Sex s= Sex.FEMALE;
-//		MaritalStatus marrSt = MaritalStatus.NOTMARRIED;
-//		int age =85;
-//		CarInsurance ci = new CarInsurance(s,marrSt,age);
-//		ci.setPremiumIns();
-//		
-//		int premium = ci.getBasePremium();
-//		assertEquals(premium,-1);
-//	}
+	@Test
+	public void test001(){
+		Sex s= Sex.MALE;
+		MaritalStatus marrSt = MaritalStatus.MARRIED;
+		int age =45;
+		CarInsurance ci = new CarInsurance(s,marrSt,age);
+		ci.setPremiumIns();
+		
+		int premium = ci.getBasePremium();
+		assertEquals(400,premium);
+	}
+	
+	@Test
+	public void test002(){
+		Sex s= Sex.MALE;
+		MaritalStatus marrSt = MaritalStatus.MARRIED;
+		int age =35;
+		CarInsurance ci = new CarInsurance(s,marrSt,age);
+		ci.setPremiumIns();
+		
+		int premium = ci.getBasePremium();
+		assertEquals(premium,300);
+	}
+	
+	@Test
+	public void test003(){
+		Sex s= Sex.MALE;
+		MaritalStatus marrSt = MaritalStatus.NOTMARRIED;
+		int age =24;
+		CarInsurance ci = new CarInsurance(s,marrSt,age);
+		ci.setPremiumIns();
+		
+		int premium = ci.getBasePremium();
+		assertEquals(premium,2000);
+	}
+	
+	@Test
+	public void test004(){
+		Sex s= Sex.FEMALE;
+		MaritalStatus marrSt = MaritalStatus.NOTMARRIED;
+		int age =85;
+		CarInsurance ci = new CarInsurance(s,marrSt,age);
+		ci.setPremiumIns();
+		
+		int premium = ci.getBasePremium();
+		assertEquals(premium,-1);
+	}
 }
